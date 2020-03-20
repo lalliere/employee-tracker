e63eb2c85b9bcff3322bf0cacbf392a13102e450
lalliere <emilylallier@yahoo.com>
2020-03-20 16:00:41 -0400

    updated which fields can be null

--- schema.sql
+++ schema.sql
@@ -8,24 +8,23 @@ CREATE TABLE employee (
   first_name VARCHAR(30) NULL,
   last_name VARCHAR(30) NULL,
   role_id INT NOT NULL,
-  manager_id INT Null,
+  manager_id INT NULL,
   PRIMARY KEY (id)
 );
 
 CREATE TABLE position (
   id INT NOT NULL AUTO_INCREMENT,
   name VARCHAR(30) NULL,
-  salary DECIMAL(9,2) NOT NULL,
+  salary DECIMAL(9,2) NULL,
   department_id INT NOT NULL,
   PRIMARY KEY (id)
 );
 
 CREATE TABLE department (
   id INT NOT NULL AUTO_INCREMENT,
-  name VARCHAR(30) NOT NULL,
+  name VARCHAR(30) NULL,
   PRIMARY KEY (id)
 );
-
 
 SELECT * FROM employee;
 SELECT * FROM position;
