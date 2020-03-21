0602d279d3058aad7f30b1232c53a59b6e48dfcd
lalliere <emilylallier@yahoo.com>
2020-03-21 13:02:30 -0400

    updated schema to have foreign keys defined

--- schema.sql
+++ schema.sql
@@ -3,29 +3,37 @@ CREATE database company_db;
 
 USE company_db;
 
-CREATE TABLE employee (
-  id INT NOT NULL AUTO_INCREMENT,
-  first_name VARCHAR(30) NULL,
-  last_name VARCHAR(30) NULL,
-  role_id INT NOT NULL,
-  manager_id INT NULL,
-  PRIMARY KEY (id)
-);
-
-CREATE TABLE position (
-  id INT NOT NULL AUTO_INCREMENT,
-  name VARCHAR(30) NULL,
-  salary DECIMAL(9,2) NULL,
-  department_id INT NOT NULL,
-  PRIMARY KEY (id)
-);
-
 CREATE TABLE department (
   id INT NOT NULL AUTO_INCREMENT,
   name VARCHAR(30) NULL,
   PRIMARY KEY (id)
 );
 
-SELECT * FROM employee;
-SELECT * FROM position;
+CREATE TABLE role (
+  id INT NOT NULL AUTO_INCREMENT,
+  name VARCHAR(30) NULL,
+  salary DECIMAL(9,2) NULL,
+  department_id INT NOT NULL,
+  PRIMARY KEY (id),
+  FOREIGN KEY (department_id) 
+  	REFERENCES department(id)
+    ON UPDATE CASCADE ON DELETE CASCADE
+);
+
+CREATE TABLE employee (
+  id INT NOT NULL AUTO_INCREMENT,
+  first_name VARCHAR(30) NULL,
+  last_name VARCHAR(30) NULL,
+  role_id INT NOT NULL,
+  manager_id INT,
+  PRIMARY KEY (id),
+  FOREIGN KEY (role_id) 
+  	REFERENCES role(id)
+    ON UPDATE CASCADE ON DELETE CASCADE,
+  FOREIGN KEY (manager_id) 
+  	REFERENCES employee(id)
+);
+
 SELECT * FROM department;
+SELECT * FROM role;
+SELECT * FROM employee;
