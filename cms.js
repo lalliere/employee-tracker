f7cd373195241224bd33d2d8aa5c7be7d92a7525
lalliere <emilylallier@yahoo.com>
2020-03-22 12:40:44 -0400

    function addRole working: gets id from dept table!

--- cms.js
+++ cms.js
@@ -61,31 +61,206 @@ function homeQuestions() {
 
 function addDRE() {
     inquirer
+    .prompt({
+        name: "action",
+        type: "rawlist",
+        message: chalk.magenta("Would you like to add a department, a role, or an employee?"),
+        choices: [
+            "Add department",
+            "Add role",
+            "Add employee"
+        ]
+    })
+    .then(function (answer) {
+        switch (answer.action) {
+            case "Add department":
+                addDepartment();
+                break
+            case "Add role":
+                addRole();
+                break
+            case "Add employee":
+                addEmployee();
+                break
+        }
+    })
+
+}
+
+function addDepartment() {
+    inquirer
         .prompt({
             name: "action",
-            type: "rawlist",
-            message: chalk.magenta("Would you like to add a department, a role, or an employee?"),
-            choices: [
-                "Add department",
-                "Add role",
-                "Add employee"
-            ]
-        })
-        .then(function (answer) {
-            switch (answer.action) {
-                case "Add department":
-                    addDepartment();
-                    break
-                case "Add role":
-                    addRole();
-                    break
-                case "Add employee":
-                    addEmployee();
-                    break
+            type: "input",
+            message: chalk.magenta("What is the name of the new department?"),
+            validate: function validateDepartmentName(name) {
+                if (name === '') {
+                    console.log(chalk.redBright("You must enter a valid department name!"));
+                    return false;
+                }
+                else {
+                    return true;
+                }
             }
         })
+        .then(function (answer) {
+            let query = "INSERT INTO department SET ?";
+            connection.query(query,
+                {
+                    name: answer.action
+                },
+                function (err, res) {
+                    if (err) console.log(err);
+                    console.log(chalk.yellow("Department added successfully!"))
+                    homeQuestions();
+                })
+        })
+}
 
-    console.log("1")
+function addRole() {
+    connection.query("SELECT * FROM department", function(err, results) {
+        if (err) throw err;
+    
+        inquirer
+            .prompt([
+                {
+                    name: "name",
+                    type: "input",
+                    message: chalk.magenta("What is the title of the new role?"),
+                    validate: function validateRoleName(name) {
+                        if (name === '') {
+                            console.log(chalk.redBright("You must enter a valid role title!"));
+                            return false;
+                        }
+                        else {
+                            return true;
+                        }
+                    }
+                },
+                {
+                    name: "salary",
+                    type: "input",
+                    message: chalk.magenta("Enter salary: "),
+                    validate: function (value) {
+                        if (isNaN(parseInt(value)) === true) {
+                            return new Error(chalk.redBright("Please delete your entry and enter a valid number!"));
+                        }
+                        else {
+                            return true;
+                        }
+                    }
+                },
+                {
+                    name: "dept",
+                    type: "rawlist",
+                    message: chalk.magenta("Which department does this role belong in?"),
+                    choices: function() {
+                        let deptArray = [];
+                        for (let i = 0; i < results.length; i++) {
+                            deptArray.push(results[i].name);
+                        }
+                        return deptArray;
+                    }
+
+                }
+            ])
+            .then(function(answer) {
+                let chosenDeptID;
+                for (let i = 0; i < results.length; i++) {
+                    if (results[i].name === answer.dept) {
+                        chosenDeptID = results[i].id
+                    }
+                }
+
+                connection.query("INSERT INTO role SET ?",
+                    {
+                        name: answer.name,
+                        salary: answer.salary,
+                        department_id: chosenDeptID
+                    
+                    },
+                    function (err, res) {
+                        if (err) console.log(err);
+                        console.log(chalk.yellow("Role added successfully!"))
+                        homeQuestions();
+                    }
+                )
+
+            })
+
+    })
+}
+    // let deptDataString = "";
+    // let deptData = [];
+
+    // connection.query("SELECT * FROM department", function(err, res) {
+    //     if (err) throw err;
+
+    //     deptDataString = JSON.stringify(res);
+    //     deptData = res;
+       
+    //     addRoleQuestions();
+    // })
+
+    
+    // function addRoleQuestions() {
+    //     inquirer
+    //     .prompt([
+    //         {
+    //             name: "name",
+    //             type: "input",
+    //             message: chalk.magenta("What is the title of the new role?"),
+    //             validate: function validateRoleName(name) {
+    //                 if (name === '') {
+    //                     console.log(chalk.redBright("You must enter a valid role title!"));
+    //                     return false;
+    //                 }
+    //                 else {
+    //                     return true;
+    //                 }
+    //             }
+    //         },
+    //         {
+    //             name: "salary",
+    //             type: "input",
+    //             message: chalk.magenta("Enter salary: "),
+    //             validate: function (value) {
+    //                 if (isNaN(parseInt(value)) === true) {
+    //                     return new Error(chalk.redBright("Please delete your entry and enter a valid number!"));
+    //                 }
+    //                 else {
+    //                     return true;
+    //                 }
+    //             }
+    //         },
+    //         {
+    //             name: "dept",
+    //             type: "rawlist",
+    //             message: chalk.magenta("Which department does this role belong in?"),
+    //             choices: deptData
+
+    //         }
+    //     ])
+    //     .then(function (answer) {
+    //         let query = "INSERT INTO role "
+    //         // let query = "INSERT INTO role SET ?";
+    //         // connection.query(query,
+    //         //     {
+    //         //         name: answer.name,
+    //         //         salary: answer.salary,
+    //         //         department_id: answer.dept
+                
+    //         //     },
+    //         //     function (err, res) {
+    //         //         if (err) console.log(err);
+    //         //         console.log(chalk.yellow("Role added successfully!"))
+    //         //         homeQuestions();
+    //         //     })
+    //     })
+    // }
+
+
+function addEmployee() {
 }
 
 function viewDRE() {
@@ -113,16 +288,27 @@ function viewDRE() {
                     break
             }
         })
-    console.log("2")
+   
+}
+
+function viewDepartments() {
+    let query = "SELECT * FROM department";
+    connection.query(query, function (err, res) {
+        if (err) console.log(err);
+        console.table(res)
+    })
+}
+function viewRoles() {
+}
+function viewEmployees() {
 }
 
 function updateEmployee() {
     console.log("3")
 }
 
-function viewEmpByMgr() {
-    console.log("4")
-}
+
+
 
 function deleteDRE() {
     inquirer
@@ -150,57 +336,6 @@ function deleteDRE() {
     console.log("5")
 }
 
-function viewBudget() {
-    console.log("6")
-}
-
-function addDepartment() {
-    inquirer
-        .prompt({
-            name: "action",
-            type: "input",
-            message: chalk.magenta("What is the name of the new department?"),
-            validate: function validateDepartmentName(name) {
-                if (name === '') {
-                    console.log(chalk.redBright("You must enter a valid department name!"));
-                    return false;
-                }
-                else {
-                    return true;
-                }
-            }
-        })
-        .then(function (answer) {
-            //console.log(answer.action)
-            let query = "INSERT INTO department SET ?";
-            connection.query(query,
-                {
-                    name: answer.action
-                },
-                function (err, res) {
-                    if (err) console.log(err);
-                    console.log(chalk.yellow("Department added successfully!"))
-                })
-        })
-}
-
-function addRole() {
-}
-
-function addEmployee() {
-}
-
-function viewDepartments() {
-    let query = "SELECT * FROM department";
-    connection.query(query, function (err, res) {
-        if (err) console.log(err);
-        console.table(res)
-    })
-}
-function viewRoles() {
-}
-function viewEmployees() {
-}
 function deleteDepartment() {
     inquirer
         .prompt({
@@ -219,4 +354,49 @@ function deleteDepartment() {
                 })
         })
 }
+
+function deleteRole() {
+    console.log("delete role")
+}
+
+function deleteEmployee() {
+    console.log("delete employee)")
+}
+
+function viewEmpByMgr() {
+    
+    let query = "SELECT employee.manager_id";
+    query += "FROM employee INNER JOIN role ON (employee.role_id = role.name AND role.department_id";
+    query += "= role.id) WHERE (employee.role_id = ? AND role.department_id = ?) ORDER BY role.id";
+
+    connection.query(query, function(err, res) {
+        if (err) console.log(err);
+        
+        for (let i = 0; i < res.length; i++) {
+            console.table(
+                i+1 + ".) " +
+                "Manager: " +
+                res[i].manager_id +
+                " Role: " +
+                res[i].role_id +
+                " Department: " +
+                res[i].department_id
+            );
+        }
+    });
+}
+
+
+function viewBudget() {
+    console.log("6")
+}
+
+
+
+
+
+
+
+
+
 
