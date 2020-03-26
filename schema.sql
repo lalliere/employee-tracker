c4ce71ceca545f2fd63898c6dda4eafada034e70
lalliere <emilylallier@yahoo.com>
2020-03-26 18:05:36 -0400

    updated FK constraints from RESTRICT to SET NULL

--- schema.sql
+++ schema.sql
@@ -5,31 +5,31 @@ USE company_db;
 
 CREATE TABLE department (
   id INT NOT NULL AUTO_INCREMENT,
-  dept_name VARCHAR(30) NULL,
+  dept_name VARCHAR(30) NOT NULL,
   PRIMARY KEY (id)
 );
 
 CREATE TABLE role (
   id INT NOT NULL AUTO_INCREMENT,
-  title VARCHAR(30) NULL,
-  salary DECIMAL(9,2) NULL,
-  department_id INT NOT NULL,
+  title VARCHAR(30) NOT NULL,
+  salary DECIMAL(9,2) NOT NULL,
+  department_id INT,
   PRIMARY KEY (id),
   FOREIGN KEY (department_id) 
   	REFERENCES department(id)
-    ON UPDATE CASCADE ON DELETE RESTRICT
+    ON UPDATE CASCADE ON DELETE SET NULL
 );
 
 CREATE TABLE employee (
   emp_id INT NOT NULL AUTO_INCREMENT,
-  first_name VARCHAR(30) NULL,
-  last_name VARCHAR(30) NULL,
-  role_id INT NOT NULL,
+  first_name VARCHAR(30) NOT NULL,
+  last_name VARCHAR(30) NOT NULL,
+  role_id INT,
   manager_id INT,
   PRIMARY KEY (emp_id),
   FOREIGN KEY (role_id) 
   	REFERENCES role(id)
-    ON UPDATE CASCADE ON DELETE RESTRICT,
+    ON UPDATE CASCADE ON DELETE SET NULL,
   FOREIGN KEY (manager_id) 
   	REFERENCES employee(emp_id)
 );
